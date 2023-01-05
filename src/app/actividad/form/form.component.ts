538b9a9e67acc006ab6a6312fd7867336d2e5ea9
jvalencia95 <jcesarval95@hotmail.com>
2023-01-05 23:56:50 +0100

    asincrona09

---
+++ src/app/actividad/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  num1 = 0
+  num2 = 0
+  selectValue = "ninguno"
+}
